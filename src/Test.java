48534788b2963a44ebcba1f8344c1eb39fcb02d4
zhangyuge <892362381@qq.com>
2019-02-01 15:34:07 +0800

    more edid。。

--- src/Test.java
+++ src/Test.java
@@ -1,19 +1,21 @@
 
 public class Test {
-		public static void main(String[] args){
-		B b=new B();
-		b.getNumber(0);
-		System.out.println(b.getNumber(0));
+	public static void main(String[] args) {
+		int a,b = 0;
+		for (int val = 0; val < 30; val += 5)
+		{
+			a = val * 8;
+			b = val / 2;
+			System.out.println(a+"\t"+b);
 		}
+		for (int val = 0; val < 30; val += 5)
+		{
+			a = val << 3;
+			b = val >> 1;
+			System.out.println(a+"\t"+b);
 		}
-
-class A{
-	public int getNumber(int a){
-	return a+1;
-	}
-	}
-	class B extends A{
-	public int getNumber(int a, char c){
-	return a+2;
 	}
 }
+
+
+
--- src/happy.java
+++ src/happy.java
@@ -3,5 +3,6 @@ public class happy {
 	public static void main(String[] args) {
 		System.out.println("test....");
 		System.out.println("哈哈。。");
+		System.out.println("是的家乐福");
 	}
 }
